786b0cb41637b692d6d85ff83faf6b37e247fd58
Prasanth <kesavarapu.siva@gmail.com>
2021-05-22 09:34:07 +0530

    dev 0.0.18 start

--- src/extension/models/constants.ts
+++ src/extension/models/constants.ts
@@ -68,7 +68,7 @@ export enum Constants {
     NOTEBOOK_ID = 'dothttp-book',
 
     // download stuff
-    EXTENSION_VERSION = "0.0.17",
+    EXTENSION_VERSION = "0.0.18",
 
     dothttpNotebook = "dothttp-book",
 
