cddbff66c4861d323a64a55f268b2937795e8bda
prasanth <kesavarapu.siva@gmail.com>
2021-05-30 21:48:53 +0530

    update version

--- src/extension/models/constants.ts
+++ src/extension/models/constants.ts
@@ -68,7 +68,7 @@ export enum Constants {
     NOTEBOOK_ID = 'dothttp-book',
 
     // download stuff
-    EXTENSION_VERSION = "0.0.19",
+    EXTENSION_VERSION = "0.0.20",
 
     dothttpNotebook = "dothttp-book",
 
