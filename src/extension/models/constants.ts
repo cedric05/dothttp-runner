85ddc03c7f8b734909b3548120668ce4c9e690d6
Prasanth <kesavarapu.siva@gmail.com>
2021-06-20 18:19:45 +0530

    make it available for older versions

--- src/extension/models/constants.ts
+++ src/extension/models/constants.ts
@@ -70,7 +70,7 @@ export enum Constants {
     NOTEBOOK_ID = 'dothttp-book',
 
     // download stuff
-    EXTENSION_VERSION = "0.0.23",
+    EXTENSION_VERSION = "0.0.24",
 
     dothttpNotebook = "dothttp-book",
 
