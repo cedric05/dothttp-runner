bfc9932bd91b09f7dd4e7261f5caceab68bd6c52
prasanth <11557066+cedric05@users.noreply.github.com>
2021-09-28 22:31:47 +0530

    Generate extend in http is not working

--- src/extension/models/constants.ts
+++ src/extension/models/constants.ts
@@ -77,7 +77,7 @@ export enum Constants {
     NOTEBOOK_ID = 'dothttp-book',
 
     // download stuff
-    EXTENSION_VERSION = "0.0.29",
+    EXTENSION_VERSION = "0.0.32",
 
     dothttpNotebook = "dothttp-book",
 
